f4898f3c503ef94b3152d881941bba56a257438f
tuemo thomas <tuemothomas@gmail.com>
2023-03-27 18:02:11 +0200

    first commit

---
+++ server/config.js
@@ -0,0 +1,12 @@
+export default {
+  JWT_SECRET: "<secret>",
+  EMAIL: "resetpassword@turbosoft-freelancer.com", // testing email & password
+  //EMAIL: "colin.gerhold@ethereal.email",
+  //PASSWORD:"2D64jw27fUJBMnPSc6",
+  PASSWORD: "Tt@123456789",
+  ATLAS_URI:
+    "mongodb+srv://tuemothomas:admin123@cluster0.b0gmihk.mongodb.net/?retryWrites=true&w=majority",
+  apiMongoDB_URL: "mongodb://localhost:27017/testDB",
+  
+  
+};
